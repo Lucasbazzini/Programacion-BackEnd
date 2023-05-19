e31fd39c0ed06f77a03d6ec689f672ca44568b06
Lucas Bazzini <lbazzini488@gmail.com>
2023-05-19 10:32:10 -0300

    trayendo archivo entregable 2 corregido

--- entregable3/src/app.js
+++ entregable3/src/app.js
@@ -1,16 +1,18 @@
 import express from 'express';
-import { pManager } from "./ProductManager.js";
+import ProductManager from "./ProductManager.js";
+
+const productManager = new ProductManager();
 
 const app = express();
 
 app.get('/products', (req, res) => {
-    const products = pManager.getProducts();
+    const products = productManager.getProducts();
     res.send(JSON.stringify(products));
 });
 
 app.get('/products/:id', (req, res) => {
     const productId = parseInt(req.params.id);
-    const product = pManager.getProductById(productId);
+    const product = productManager.getProductById(productId);
     if (product) {
         res.send(JSON.stringify(product));
     } else {

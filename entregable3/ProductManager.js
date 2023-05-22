f821cc1e7c8c3eb7f5d27cfe2974123c215ed072
Lucas Bazzini <lbazzini488@gmail.com>
2023-05-22 11:37:02 -0300

    entregable 3 listo

--- entregable3/ProductManager.js
+++ entregable3/ProductManager.js
@@ -1,95 +1,97 @@
-import fs from 'fs'
+import fs from 'fs';
 
-export default class ProductManager {
-    constructor(path) {
-        this.products = [];
-        this.id = 1;
-        this.path = path;
-    }
+class ProductManager {
+  constructor(path) {
+    this.products = [];
+    this.id = 1;
+    this.path = path;
+    this.loadProducts(); 
+  }
 
-    addProduct(title, description, price, url, code, stock) {
-        if (title && description && price && url && code && stock) {
-            const verificationCode = this.products.some(product => product.code === code);
-            if (verificationCode) {
-                console.error("ERROR: El codigo está repetido");
-            } else {
-                let id = this.id++;
-                const newProduct = { id, title, description, price, url, code, stock };
-                this.products.push(newProduct);
-                console.log("Producto agregado correctamente");
-            }
-        } else {
-            console.error("ERROR: Debe completar todos los campos");
-        }
+  addProduct(title, description, price, url, code, stock) {
+    if (title && description && price && url && code && stock) {
+      const verificationCode = this.products.some(product => product.code === code);
+      if (verificationCode) {
+        console.error('ERROR: El código está repetido');
+      } else {
+        let id = this.id++;
+        const newProduct = { id, title, description, price, url, code, stock };
+        this.products.push(newProduct);
+        console.log('Producto agregado correctamente');
+        this.archivarProds();
+      }
+    } else {
+      console.error('ERROR: Debe completar todos los campos');
     }
+  }
 
-    deleteProduct(id) {
-        const index = this.products.findIndex(product => product.id === id);
-        if (index === -1) {
-            console.error("No se encontró ningún producto con ese ID");
-            return;
-        }
-        const deletedProduct = this.products.splice(index, 1);
-        console.log("Producto eliminado correctamente:", deletedProduct);
+  deleteProduct(id) {
+    const index = this.products.findIndex(product => product.id === id);
+    if (index === -1) {
+      console.error('No se encontró ningún producto con ese ID');
+      return;
     }
+    const deletedProduct = this.products.splice(index, 1);
+    console.log('Producto eliminado correctamente:', deletedProduct);
+    this.archivarProds();
+  }
 
-    updateProduct(id, newObject) {
-        const productIndex = this.products.findIndex(product => product.id === id);
-        if (productIndex === -1) {
-            console.error("No se encontró el producto");
-            return;
-        }
-        const updatedProduct = {
-            ...this.products[productIndex],
-            ...newObject
-        };
-        this.products[productIndex] = updatedProduct;
-        console.log("Producto actualizado correctamente");
+  updateProduct(id, newObject) {
+    const productIndex = this.products.findIndex(product => product.id === id);
+    if (productIndex === -1) {
+      console.error('No se encontró el producto');
+      return;
     }
+    const updatedProduct = {
+      ...this.products[productIndex],
+      ...newObject
+    };
+    this.products[productIndex] = updatedProduct;
+    console.log('Producto actualizado correctamente');
+    this.archivarProds();
+  }
 
-    getProducts() {
-        return this.products;
+  getProducts(limit) {
+    if (limit) {
+      return this.products.slice(0, limit); 
     }
+    return this.products; 
+  }
 
-    getProductById(id) {
-        const product = this.products.find(product => product.id === id);
-        if (!product) {
-            console.error("No se encontró el producto");
-            return;
-        }
-        console.log("Producto con el ID solicitado:", product);
+  getProductById(id) {
+    const product = this.products.find(product => product.id === id);
+    if (!product) {
+      console.error('No se encontró el producto');
+      return;
     }
+    console.log('Producto con el ID solicitado:', product);
+    return product;
+  }
 
-    archivarProds() {
-        const jsonData = JSON.stringify(this.products);
-        fs.writeFileSync(this.path, jsonData, (error) => {
-            if (error) {
-                console.error(error);
-            } else {
-                console.log("Productos archivados correctamente");
-                fs.readFile(this.path, "utf-8", (error, resultado) => {
-                    if (error) {
-                        console.error(error);
-                    } else {
-                        console.log(resultado);
-                    }
-                });
-            }
-        });
+  loadProducts() {
+    try {
+      const data = fs.readFileSync(this.path, 'utf-8');
+      this.products = JSON.parse(data);
+      console.log('Productos cargados correctamente');
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        console.log('El archivo no existe. Se creará uno nuevo.');
+        this.archivarProds(); 
+      } else {
+        console.error(error);
+      }
     }
+  }
+
+  archivarProds() {
+    try {
+      const jsonData = JSON.stringify(this.products);
+      fs.writeFileSync(this.path, jsonData);
+      console.log('Productos archivados correctamente');
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
 
-export const pManager = new ProductManager('./productos.json');
-
-pManager.addProduct("Fideos", "con tuco", 20, "url", 123, 25);
-pManager.addProduct("Arroz", "con atún", 20, "url", 124, 25);
-pManager.addProduct("Milanesas", "con puré", 20, "url", 125, 25);
-pManager.addProduct("Hamburguesa", "con queso", 20, "url", 126, 25);
-pManager.addProduct("fideos", "con queso", 20, "url", 127, 25);
-pManager.addProduct("fideos", "con queso", 20, "url", 128, 25);
-pManager.updateProduct(4, { title: "Papas", description: "Papas fritas", price: 70, url: "google.com/fideos", code: 135, stock: 24 });
-console.log(pManager.getProducts());
-
-
-
-pManager.archivarProds();
+export default ProductManager;
--- entregable3/app.js
+++ entregable3/app.js
@@ -1,25 +1,31 @@
 import express from 'express';
-import ProductManager from "./ProductManager.js";
+import ProductManager from './ProductManager.js';
 
-const productManager = new ProductManager();
+const productManager = new ProductManager('./productos.json');
 
 const app = express();
 
 app.get('/products', (req, res) => {
-    const products = productManager.getProducts();
-    res.send(JSON.stringify(products));
+  const limit = req.query.limit; 
+  let products = productManager.getProducts();
+
+  if (limit) {
+    products = products.slice(0, parseInt(limit)); 
+  }
+
+  res.send(JSON.stringify(products));
 });
 
 app.get('/products/:id', (req, res) => {
-    const productId = parseInt(req.params.id);
-    const product = productManager.getProductById(productId);
-    if (product) {
-        res.send(JSON.stringify(product));
-    } else {
-        res.send("Producto no encontrado");
-    }
+  const productId = parseInt(req.params.id);
+  const product = productManager.getProductById(productId);
+  if (product) {
+    res.send(JSON.stringify(product));
+  } else {
+    res.send('Producto no encontrado');
+  }
 });
 
 app.listen(8080, () => {
-    console.log("Servidor escuchando en el puerto 8080");
+  console.log('Servidor escuchando en el puerto 8080');
 });
